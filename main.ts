063fc58d03f99c01d41f600f9b571ffbb4bd65e0
atlmapper <ngalford@gmail.com>
2023-10-04 18:25:52 -0400

    v3.0 Review #2 Finished

--- main.ts
+++ main.ts
@@ -2,6 +2,9 @@
 This is the entry point of the web application. It creates a new game instance, sets the configurations, and starts the game.
 '''
 import { Game } from './game';
+import { Space } from './space';
+import { Player } from './player';
+import { Device } from './device';
 // Create a new game
 const game = new Game();
 // Set configurations for the game
